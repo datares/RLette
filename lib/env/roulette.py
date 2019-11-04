498ed8042563373ac69619d3bc3231bfa3e6bccf
qjyap <qijingyap@yahoo.com.sg>
2019-11-04 00:03:26 -0800

    added render to roulette to create an output environment

--- lib/env/roulette.py
+++ lib/env/roulette.py
@@ -8,28 +8,39 @@ class  new_roulette(gym.Env):
 		self.action_space = spaces.Discrete(self.n)
 		self.observation_space = spaces.Discrete(1)
 		self.seed()
-		self.budget = 0
+		self.budget = 100
+		self.reward = 0
+		self.val = 'placeholder'
+		self.bet = 'placeholder'
+
 	def  seed(self, seed=None):
 		self.np_random, seed = seeding.np_random(seed)
 		return  [seed]
 	def  step(self, action):
 		assert  self.action_space.contains(action)
+		self.bet = action
 		if action ==  self.n -  1:
 			# observation, reward, done, info
 			return  0,  0,  True, {}, self.budget
 		# N.B. np.random.randint draws from [A, B) while random.randint 		draws from [A,B]
-		val =  self.np_random.randint(0,  self.n -  1)
-		if val == action:
-			reward = 15
-		if val == action ==  0:
-			reward =  self.n -  2.0
-		elif val !=  0  and action !=  0  and val %  2  == action %  2:
-			reward =  1.0
+		self.val =  self.np_random.randint(0,  self.n -  1)
+		if self.val == action:
+			self.reward = 15
+		if self.val == action ==  0:
+			self.reward =  self.n -  2.0
+		elif self.val !=  0  and action !=  0  and self.val %  2  == action %  2:
+			self.reward =  1.0
 		else:
-			reward =  -1.0
+			self.reward =  -1.0
 
-		self.budget = self.budget+reward
-		return  0, reward,  False, {}, self.budget, val, action
+		return  0, self.reward,  False, {}, self.budget, self.val, action
+    
+	def render(self):
+        # Render the environment to the scsreen
+		self.budget = self.budget + self.reward
+		print('You bet on ' + str(self.bet) + '. The roulette ball has landed on ' + str(self.val) + '.')
+		print('You earned ' + str(self.reward) + ' in the previous round.')
+		print('Your current budget is ' + str(self.budget))
 
 	def  reset(self):
 		return  0
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     
 env.reset()
 
-for _ in  range(100):
-    print(env.step(env.action_space.sample()))  # take a random action
-
+for _ in  range(1000):
+    env.step(env.action_space.sample())  # take a random action
+    env.render()
 env.close()
