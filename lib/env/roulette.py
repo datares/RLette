7167c0b72df003e6e3be3135da789992f370ffe2
qjyap <qijingyap@yahoo.com.sg>
2019-11-08 18:21:12 -0800

    added array for action+state, TODO: change Q-learn to DQN

--- lib/env/roulette.py
+++ lib/env/roulette.py
@@ -1,46 +1,57 @@
 import gym
 import numpy as np
-import roulette
-#env = gym.make('Roulette-v0')
-RLette=roulette.n
-
-import gym
-
 from gym import spaces
 from gym.utils import seeding
 
 class new_roulette(gym.Env):
 	def  __init__(self, spots=37):
 		self.n = spots +  1
-		self.action_space = spaces.Discrete(self.n)
-		self.observation_space = spaces.Discrete(1)
+		self.action_space = spaces.Box(low=0, high=1, shape=
+		(7,), dtype=np.int32)
+		self.observation_space = spaces.Box(low=0, high=10000,shape = (9,), dtype=np.int32)
 		self.seed()
 		self.budget = 500
 	def  seed(self, seed=None):
 		self.np_random, seed = seeding.np_random(seed)
 		return  [seed]
 	def  step(self, action):
-		assert  self.action_space.contains(action)
-		if action ==  self.n -  1:  
-			# observation, reward, done, info
-			return  0,  0,  True, {}
-
+		#assert  self.action_space.contains(action)
+		print(action)
 		#	Pick a value for the roulette 
 		val =  self.np_random.randint(0,  self.n -  1)
-		
-		if val == action ==  0:
-			reward =  self.n -  2.0
-		elif val !=  0  and action !=  0  and val %  2  == action %  2:
-			reward = 1.0
-		else:
-			reward = -1.0
-		
-		#	COULD INTRODUCE REWARD CLIPPING
+		obs = [val, 
+			self.budget, 
+			val % 2 == 0, 
+			val % 2 != 0, 
+			val >= 1 and val <= 12, 
+			val >= 13 and val <= 24, 
+			val >= 25 and val <= 36,
+			val >= 1 and val <= 18, 
+			val >= 19 and val <= 36]
+
+		reward = 0
+		for i in range(len(obs) - 2):
+			if obs[i + 2] and action[i]:
+				reward += 1
+			if not obs[i + 2] and action[i]:
+				reward -= 1
 
 		self.budget = self.budget + reward
-		return  0, reward, self.budget < 100, {}
+		obs = [val, 
+		self.budget, 
+		val % 2 == 0, 
+		val % 2 != 0, 
+		val >= 1 and val <= 12, 
+		val >= 13 and val <= 24, 
+		val >= 25 and val <= 36,
+		val >= 1 and val <= 18, 
+		val >= 19 and val <= 36]
+
+        #print("NUMBER, BUDGET, EVEN, ODD, Ist THIRD, IInd THIRD, IIIrd THIRD, 1ST Half, 2nd Half")
+     
+		return  obs, reward, self.budget < 100, {}
 
 	def  reset(self):
 		self.budget = 500
-		return 0
+		return [0,self.budget,0,0,0,0,0,0,0]
 	
